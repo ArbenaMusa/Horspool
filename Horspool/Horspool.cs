dc3f04fe3eceb800ad3148aeffd461a19ae33922
Arbena Musa <arbena.musa@student.uni-pr.edu>
2020-12-27 21:38:48 +0100

    Implemented HorsePool Algorithm

---
+++ Horspool/Horspool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Horspool
+{
+    class Horspool
+    {
+
+        public static int SIZE = 500;
+        public static int[] table = new int[SIZE];
+
+
+        public void shifttable(string pattern)
+        {
+            char[] patternArray = pattern.ToCharArray();
+            int m = pattern.Length;
+
+            for (int i = 0; i < SIZE; i++)
+                table[i] = m;
+
+            for (int j = 0; j < m - 1; j++)
+                table[patternArray[j]] = m - 1 - j;
+        }
+        public int horspool(string source, string pattern)
+        {
+            int k;
+            char[] sourceArray = source.ToCharArray();
+            char[] patternArray = pattern.ToCharArray();
+            int m = pattern.Length;
+
+            int i = m - 1;
+
+            while(i < source.Length)
+            {
+                k = 0;
+                while ((k < m) && (patternArray[m - 1 - k] == sourceArray[i - k]))
+                    k++;
+                if (k == m)
+                    return i - m + 1;
+                else
+                    i += table[sourceArray[i]];
+            }
+            return -1;
+        }
+    }
+}
---
+++ Horspool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Horspool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position;
+            Horspool horsepool = new Horspool();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Give the source text:");
+                string source = Console.ReadLine();
+                Console.WriteLine("Give the pattern:");
+                string pattern = Console.ReadLine();
+
+                horsepool.shifttable(pattern);
+                position = horsepool.horspool(source, pattern);
+                if (position == -1)
+                    Console.WriteLine("Pattern not found!");
+                else
+                    Console.WriteLine("Pattern found at: " + position);
+            }
+        }
+    }
+}
